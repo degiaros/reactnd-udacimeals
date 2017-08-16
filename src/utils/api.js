e910b14c1bf82823f15863365909fd339f690569
SamDegDev <samdegdev@outlook.com>
2017-08-16 22:33:39 +0100

    chore: changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
-const API_ID = process.env.REACT_APP_API_ID
-const APP_KEY = process.env.REACT_APP_APP_KEY
+const API_ID = 'b5ac4d2e'//process.env.REACT_APP_API_ID
+const APP_KEY = '04562f61d22f22a33c6db556f1c0b7a0'//process.env.REACT_APP_APP_KEY
 
 export function fetchRecipes (food = '') {
   food = food.trim()
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,2 @@
+/// <reference path="globals/react-icons/index.d.ts" />
 /// <reference path="globals/redux/index.d.ts" />
